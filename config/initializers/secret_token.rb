08121840cdc04ab852311fd24c6fbe146919a677
Nic <nicole.dun@gmail.com>
2011-04-27 08:30:54 -0700

    new book swap app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookSwap::Application.config.secret_token = '79d1b7b3427db7ba2d61712cd1b82ab2e5a17db955a80b1c72b13c747bedc08ddff9455a95bb0bc6b0339b09dc07c530b6edbd14ed01eaf74f9ab38a1dc09275'
